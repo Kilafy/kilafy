ae244fbec350a2dda66cdd33fe6951ae943c393f
andrescaro16 <julicaro2003@gmail.com>
2024-06-16 18:48:43 -0500

    Add PhotoFrame
    First approach/version to PhotoFrame component. It currently supports 1 'frame' per image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import PhotoFrame from "./components/PhotoFrame";
 
 const App = () => {
   return (
@@ -7,6 +8,26 @@ const App = () => {
       <div className="pt-[71px] lg:pt-[79px] bg-color-2">
         <Header />
         <Hero />
+        <div className="h-full w-full flex justify-center items-center p-10">
+          <PhotoFrame
+            imgSrc="/assets/images/IMG_19.webp"
+            imgWidth="720px"
+            imgHeight="480px"
+            frameRounded="rounded-lg"
+            frameColor="bg-color-4"
+            framePosition="bottomRight"
+          />
+        </div>
+        <div className="h-full w-full flex justify-center items-center p-10">
+          <PhotoFrame
+            imgSrc="/assets/images/team/Andres_Caro.webp"
+            imgWidth="720px"
+            imgHeight="720px"
+            frameRounded="rounded-lg"
+            frameColor="bg-color-1"
+            framePosition="topLeft"
+          />
+        </div>
       </div>
     </>
   );
---
+++ src/components/PhotoFrame.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable react/prop-types */
+
+/**
+ * Renders a photo frame component with customizable options.
+ *
+ * @component
+ * @example
+ * // Example usage of PhotoFrame component
+ * <PhotoFrame
+ *   imgSrc="/path/to/image.webp"
+ *   imgWidth="720px"
+ *   imgHeight="480px"
+ *   frameRounded="rounded-lg"
+ *   frameColor="bg-color-1"
+ *   framePosition="topRight"
+ * />
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.imgSrc - The source URL of the image to be displayed in the frame.
+ * @param {string} props.imgHeight - The height of the image in pixels.
+ * @param {string} props.imgWidth - The width of the image in pixels.
+ * @param {string} [props.frameRounded="rounded-lg"] - The Tailwind CSS class for the rounded corners of the frame.
+ * @param {string} [props.frameColor="bg-color-1"] - The Tailwind CSS class for the background color of the frame.
+ * @param {string} [props.framePosition="topLeft"] - The position of the frame within the container. Possible values are "topLeft", "topRight", "bottomLeft", and "bottomRight".
+ *
+ * @returns {JSX.Element} The rendered PhotoFrame component.
+ */
+
+import "./design/css/PhotoFrameShadows.css";
+
+const positionClasses = {
+  topLeft: {
+    position: "top-[-20px] left-[-20px]",
+    margin: "mt-[20px] ml-[20px]",
+    shadow: "shadow-top-left",
+  },
+  topRight: {
+    position: "top-[-20px] right-[-20px]",
+    margin: "mt-[20px] mr-[20px]",
+    shadow: "shadow-top-right",
+  },
+  bottomLeft: {
+    position: "bottom-[-20px] left-[-20px]",
+    margin: "mb-[20px] ml-[20px]",
+    shadow: "shadow-bottom-left",
+  },
+  bottomRight: {
+    position: "bottom-[-20px] right-[-20px]",
+    margin: "mb-[20px] mr-[20px]",
+    shadow: "shadow-bottom-right",
+  },
+};
+
+const PhotoFrame = ({
+  imgSrc,
+  imgHeight,
+  imgWidth,
+  frameRounded = "rounded-lg",
+  frameColor = "bg-color-1",
+  framePosition = "topLeft",
+}) => {
+  const { position, margin } = positionClasses[framePosition];
+  const { shadow } = positionClasses[framePosition];
+
+  return (
+    <div
+      style={{ height: imgHeight, width: imgWidth }}
+      className={`relative z-30 ${margin}`}>
+      <div
+        className={`absolute w-full h-full bg-cover shadow-black ${frameRounded} ${shadow}`}
+        style={{ backgroundImage: `url('${imgSrc}')`, zIndex: 40 }}
+      />
+      <div
+        className={`absolute w-full h-full ${position} ${frameColor} ${frameRounded}`}
+        style={{ zIndex: 30 }}
+      />
+    </div>
+  );
+};
+
+export default PhotoFrame;
