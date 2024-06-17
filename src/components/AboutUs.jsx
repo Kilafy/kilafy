ae6431c0e0c8cdf2e917f96682afecdc1986a6ad
andrescaro16 <julicaro2003@gmail.com>
2024-06-17 11:58:22 -0500

    Redesign AboutUs section
    Change title styles

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -4,7 +4,7 @@ import { aboutUsContent } from "../constants";
 
 const renderTeamMember = () => {
   return (
-    <div className="flex flex-wrap gap-6 items-center justify-center w-full h-full my-16 md:my-36">
+    <div className="flex flex-wrap gap-6 items-center justify-center w-full h-full my-16 mt-0 md:my-20 md:mt-0">
       {aboutUsContent.team.map((member) => (
         <div
           key={member.id}
@@ -30,28 +30,32 @@ const renderTeamMember = () => {
 const AboutUs = () => {
   return (
     <>
-      <h2 id="nosotros" className="h2 text-center w-full p-4 bg-color-4">
-        {aboutUsContent.title}
-      </h2>
-      <div className="flex flex-col items-center justify-center w-full h-full lg:flex-row lg:h-[500px]">
-        <img
-          src={aboutUsContent.sideImgSrc}
-          alt={aboutUsContent.sideImgAlt}
-          className="bg-color-2 lg:w-1/2 lg:h-full object-cover"
-        />
-        <p className="p bg-color-1 p-7 leading-6 lg:flex lg:items-center lg:h-full lg:w-1/2 lg:p-10 xl:px-16 lg:leading-10">
-          {aboutUsContent.description}
-        </p>
-      </div>
-
-      {renderTeamMember()}
-
-      <div className="flex w-full justify-between gap-3 px-4 mb-16 md:mb-36 md:justify-center md:gap-6 lg:gap-12 xl:gap-16">
-        <div className="flex justify-center items-center aspect-square w-1/3 max-h-[324px] max-w-[324px] p-12 bg-color-1 rounded-lg md:p-20 md:w-1/2">
-          <img src={cross}/>
+      <section id="nosotros" className="flex flex-col">
+        <div className="flex flex-col items-center justify-center w-full h-full lg:flex-row lg:h-[500px]">
+          <img
+            src={aboutUsContent.sideImgSrc}
+            alt={aboutUsContent.sideImgAlt}
+            className="bg-color-2 lg:w-1/2 lg:h-full object-cover"
+          />
+          <p className="p bg-color-1 p-7 leading-6 lg:flex lg:items-center lg:h-full lg:w-1/2 lg:p-10 xl:px-16 lg:leading-10">
+            {aboutUsContent.description}
+          </p>
         </div>
-        <p className="p flex items-center w-2/3 md:w-1/2 md:leading-6 lg:leading-10">{aboutUsContent.teamDescription}</p>
-      </div>
+        <h2 className="h2 text-center w-full p-4 my-16 md:my-20">
+          {aboutUsContent.title}
+        </h2>
+
+        {renderTeamMember()}
+
+        <div className="flex w-full justify-between gap-3 px-4 mb-16 md:mb-20 md:justify-center md:gap-6 lg:gap-12 xl:gap-16">
+          <div className="flex justify-center items-center aspect-square w-1/3 max-h-[324px] max-w-[324px] p-12 bg-color-1 rounded-lg md:p-20 md:w-1/2">
+            <img src={cross} />
+          </div>
+          <p className="p flex items-center w-2/3 md:w-1/2 md:leading-6 lg:leading-10">
+            {aboutUsContent.teamDescription}
+          </p>
+        </div>
+      </section>
     </>
   );
 };
