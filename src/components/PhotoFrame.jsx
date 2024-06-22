091f34280fae8f0d6cc13807fea6b859fea9cfb4
andrescaro16 <julicaro2003@gmail.com>
2024-06-22 16:28:24 -0500

    Testing
    Co-authored-by: FelipeArredond <pipechiqui77@gmail.com>

--- src/components/PhotoFrame.jsx
+++ src/components/PhotoFrame.jsx
@@ -76,11 +76,10 @@ const PhotoFrame = ({
     <div
       style={{ height: imgHeight, width: imgWidth }}
       className={`relative z-30 ${margin}`}>
-        {
-          framePosition === "bottomLeftTopRight" ?
-           (<div className={`absolute flex justify-center items-center w-full h-full bg-cover shadow-black bg-red-800 top-[-20px] right-[-20px] ${frameRounded} ${shadow} ${animationClass}`}></div>)
-            : null
-          }
+      {framePosition === "bottomLeftTopRight" ? (
+        <div
+          className={`absolute flex justify-center items-center w-full h-full bg-cover shadow-black bg-red-800 top-[-20px] right-[-20px] ${frameRounded} ${shadow} ${animationClass}`}></div>
+      ) : null}
       <div
         className={`absolute flex justify-center items-center w-full h-full bg-cover shadow-black ${frameRounded} ${shadow} ${animationClass}`}
         style={{ backgroundImage: `url('${imgSrc}')`, zIndex: 40 }}>
